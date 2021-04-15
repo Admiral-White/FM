a84f2c30d4a98a0c6f271ddbe7a7c70f6df5c44c
Admiral-White <mikeinyang975@gmail.com>
2021-04-15 23:01:43 +0100

    setup config file added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FM',
+    version='1.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Michael Inyang',
+    author_email='mikeinyang975@gmail.com',
+    description='backend project for farm mart'
+)
